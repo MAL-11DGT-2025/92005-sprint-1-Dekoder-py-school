d0f4510c3419bd719ca0803da093ce0875050994
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-07-24 15:58:32 +1200

    add death sound effect

--- scenes/killzone/killzone.gd
+++ scenes/killzone/killzone.gd
@@ -1,18 +1,27 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var death_sound: AudioStreamPlayer = $DeathSound
 
 
 func _on_body_entered(body: Node2D) -> void:
-	# prevent colliding again
+	# Disable player collision - player falls through map
 	body.set_process(false)
 	body.get_node("CollisionShape2D").queue_free()
 	
-	# slow down the game
+	
+	
+	# Slow down the game
 	Engine.time_scale = 0.2
+	
+	# Play death sound
+	death_sound.play()
+
+	# After sound ends, start a short timer
+	await death_sound.finished
 	timer.start()
 
-func _on_timer_timeout() -> void:
-	# after time ends, reset time scale and reload level
+func _on_timer_timeout():
+	# reset time scale and reload scene
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
