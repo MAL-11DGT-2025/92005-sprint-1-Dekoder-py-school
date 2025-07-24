b9761f7727c4e3057bec7244fd2bdc6ad02b25be
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-07-24 15:13:26 +1200

    add comment

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		# Play jump sound
 		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
