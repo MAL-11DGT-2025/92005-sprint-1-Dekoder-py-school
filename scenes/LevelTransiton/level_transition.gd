fac29e81a2c32c5630220c40cd55485e0ea9523a
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-07-24 15:13:22 +1200

    add level up sound and slow time during level transition

--- scenes/LevelTransiton/level_transition.gd
+++ scenes/LevelTransiton/level_transition.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
 @export var next_level_scene := "res://scenes/level_one/level_one.tscn"
+@onready var level_up_sound := $LevelUpSound
 
 
-# called when any collision object enters the area2D
+# Called when any collision object enters the area2D
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	# change scene to next level
+	# hide so user cannot hit the diamond twice
+	$".".visible = false
+
+	# Play level sound effect
+	level_up_sound.play()
+	
+	# Slow motion
+	Engine.time_scale = 0.5
+	
+	# Wait for sound to finish
+	await level_up_sound.finished
+	
+	# Resume normal time scale
+	Engine.time_scale = 1
+	
+	# Change scene to next level
 	get_tree().change_scene_to_file(next_level_scene)
+	
+	
 
 	
