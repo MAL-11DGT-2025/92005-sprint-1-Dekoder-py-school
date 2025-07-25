03c932eab9d0d38b8078e006e5b6a87f654f69f7
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-07-25 12:57:10 +1200

    add coins

---
+++ scenes/coin/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	animation_player.play("pickup")
+	print("+1 Coin")
+	
