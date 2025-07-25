e12551ebe4da8d43dbf4a104a8fdefaf81799226
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-07-25 13:08:30 +1200

    add custom keymaps for movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,14 +13,14 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		# Play jump sound
 		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		
