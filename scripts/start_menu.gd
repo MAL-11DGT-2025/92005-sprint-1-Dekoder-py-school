1de4daba0a6ba87a29c190c3827a5db991945644
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-08-21 14:59:20 +1200

    add a quit button and move buttons into a vbox container for alignment

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-@onready var start_button: Button = $StartButton
+@onready var button_container = $ButtonContainer
 @onready var game_title: Label = $GameTitle
 
 func get_center(node) -> Vector2:
@@ -9,8 +9,8 @@ func get_center(node) -> Vector2:
 
 func _ready() -> void:
 	# center start menu items
-	start_button.position = get_center(start_button)
+	button_container.position = get_center(button_container)
 	game_title.position = get_center(game_title)
 	# move them into the correct y-axis position
-	start_button.position.y += 60
+	button_container.position.y += 60
 	game_title.position.y -= 60
